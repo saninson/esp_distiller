05fc4fa3ae6568b622d165c8d4216d46e5e74ba7
Saninson <saninson@exten.kz>
2022-09-02 19:34:22 +0200

    added distillation programs

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,17 @@
 #include <rm.h>
 #include <Button2.h>
 #include <relay.h>
+// #ifdef WITH_NETWORK
+// #include "net.h"
+// #endif
+
 #ifdef WITH_NETWORK
-#include "net.h"
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+const char* ssid = "FRANTSOVA";
+const char* password = "218506oleg";
 #endif
 
 // MCP3008 channels
@@ -263,6 +272,55 @@ void program_A2(){
     rm.setVo(dstU);
     ms = millis();
 }
+
+#ifdef WITH_NETWORK
+void initWiFI(){
+    // WiFi & OTA
+    Serial.println("Init WiFi...");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("WiFi init failed. WiFi won't use");
+    } else {
+        Serial.println("WiFi connected");
+        Serial.print("IP: ");
+        Serial.println(WiFi.localIP());
+
+        ArduinoOTA
+            .onStart([]() {
+                String type;
+                if (ArduinoOTA.getCommand() == U_FLASH)
+                    type = "sketch";
+                else // U_SPIFFS
+                    type = "filesystem";
+
+                // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                Serial.println("Start updating " + type);
+            })
+            .onEnd([]() {
+                Serial.println("\nEnd");
+            })
+            .onProgress([](unsigned int progress, unsigned int total) {
+                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+            })
+            .onError([](ota_error_t error) {
+                Serial.printf("Error[%u]: ", error);
+                if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                else if (error == OTA_END_ERROR) Serial.println("End Failed");
+            });
+
+        ArduinoOTA.begin();
+        Serial.println("OTA ready");
+    }
+}
+
+void initMqtt(){
+
+}
+#endif
 
 void loop(){
     #ifdef WITH_NETWORK
--- src/net.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef net_h
-#define net_h
-
-const char* ssid = "FRANTSOVA";
-const char* password = "218506oleg";
-
-#endif
